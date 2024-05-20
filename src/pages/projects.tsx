fe8be8ebf49b9229b9b6d58582ab0fcc584e4881
rmrahman430 <rayyanmrahman@gmail.com>
2024-05-20 14:20:47 -0500

    projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,14 @@
+// projects.jsx
+
+import React from 'react';
+
+const Projects = () => {
+  // ... your project content
+  return (
+    <div>
+      {/* Your project components or content */}
+    </div>
+  );
+};
+
+export default Projects; // Add the default export
